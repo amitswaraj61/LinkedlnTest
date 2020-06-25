baead859ec46d020c2a21cd70f037be27d84d682
amitswaraj61 <aswaraj78@gmail.com>
2020-06-25 16:27:28 +0530

    Add Automation script to generate Extent Report

--- TestCase/LoginTestCase/LoginTest.cs
+++ TestCase/LoginTestCase/LoginTest.cs
@@ -1,4 +1,6 @@
 ﻿
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using LinkedinApp.Pages;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -14,21 +16,38 @@ namespace LinkedinApp.LoginTestCase.TestCase
   
     class LoginTest :LinkedinBase
     {
-       Credentails credentails = new Credentails();
 
+        Credentails credentails = new Credentails();
+        public ExtentReports extent = null;
+        public ExtentTest test = null;
+
+        [OneTimeSetUp]
+        public void ExtentReport()
+        {
+            extent = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"C:\Users\Kis\source\repos\LinkedinApp\LinkedinApp\Report\Report.html");
+            extent.AttachReporter(htmlReporter);
+        }
+
+        [OneTimeTearDown]
+        public void ExtentClose()
+        {
+            extent.Flush();
+        }
 
         [Test]
         public void ValidLogin()
         {
-
-           
-
+            test=extent.CreateTest("ValidLogin").Info("Test Started");
             LoginPageObject login = new LoginPageObject(driver);
-            login.LoginToLinkedin(credentails.email,credentails.password);
-            String actualResult = driver.Title;
-            String expectedResult = "LinkedIn";
-            Assert.AreEqual(expectedResult,actualResult);
+            test.Log(Status.Info,"Chrome Browser Launched");
+            login.LoginToLinkedin(credentails.email, credentails.password);
+            test.Log(Status.Info,"Valid email and password is entered");
+            String actualResult = driver.Url;
+            test.Log(Status.Info,"get the URl of Linkedin");
+            String expectedResult = "https://www.linkedin.com/feed/";
+            Assert.AreEqual(expectedResult, actualResult);
+            test.Log(Status.Pass,"Test valid Login passed");
         }
-
     }
 }
